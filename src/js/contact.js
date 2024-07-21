9d825540a16d643ec52e0e073100069d3710e777
khaisendefo <khaisendefo@gmail.com>
2024-07-21 10:51:03 +0300

    Created contact: get-started sections

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,6 +4,8 @@ import '../assets/scss/contact.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
 
 useTheme();
 useBurger();
+usePhone();
